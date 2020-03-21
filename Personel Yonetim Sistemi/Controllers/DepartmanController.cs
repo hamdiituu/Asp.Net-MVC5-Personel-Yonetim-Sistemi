1f889d9310c72968fb9298712384e8757f5b6466
Hamdi Tuğ <hamditugmobil@gmail.com>
2020-03-21 22:34:53 +0300

    Proje alt yapısı hazırlandı.

---
+++ Personel Yonetim Sistemi/Controllers/DepartmanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Personel_Yonetim_Sistemi.Controllers
+{
+    public class DepartmanController : Controller
+    {
+        // GET: Departman
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Personel Yonetim Sistemi/Controllers/PersonelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Personel_Yonetim_Sistemi.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
